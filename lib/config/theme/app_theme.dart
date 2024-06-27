65cd71209f53fd81c2c0dc74a960b966e93b99c0
AMendoza011003 <amendoza@becompliance.es>
2024-06-26 18:45:15 -0600

    Temas

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.black,
+  Colors.green
+];
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({
+    required this.selectedColor
+  }) : assert(
+    selectedColor >= 0, 'Selecciona un color mayor o igual a 0'
+  ), assert(
+    selectedColor < colorList.length, 'El color seleccionado debe ser menor a ${colorList.length}'
+  );
+
+  ThemeData getTheme() => ThemeData(
+    colorSchemeSeed: colorList[selectedColor]
+  );
+}
